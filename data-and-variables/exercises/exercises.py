2d9a0ee4604cbd8cce8221cd446941e297d7099e
josh <joharger94@gmail.com>
2024-08-13 17:31:42 -0500

    exercises, updated

--- data-and-variables/exercises/exercises.py
+++ data-and-variables/exercises/exercises.py
@@ -1,7 +1,18 @@
 # 1. Declare and assign the variables here:
-
+shuttle = "Determination"
+speed_mph = 17500
+mars_dist_km = 225000000
+moon_dist_km = 384400
+miles_pr_km = 0.621
 # 2. Use print() to print the 'type' each variable. Print one item per line.
-
+print(type(shuttle))
+print(type(speed_mph))
+print(type(mars_dist_km))
+print(type(moon_dist_km))
+print(type(miles_pr_km))
 # Code your solution to exercises 3 and 4 here:
-
+miles_to_mars = mars_dist_km * miles_pr_km
+hours_to_mars = miles_to_mars / speed_mph
+days_to_mars = hours_to_mars / 24 
 # Code your solution to exercise 5 here
+print(shuttle + " will take " + str(days_to_mars) + " days to reach the Moon.")
---
+++ how-to-write-code/print-exmples.py
@@ -0,0 +1,8 @@
+print('Hello, Python')
+day = "Thursday"
+day = 32.5
+day = 19
+print(day)
+print(2 + 2)
+info = input("Please Enter Your Age: ")
+print(type(info))
